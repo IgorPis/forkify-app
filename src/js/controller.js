f3802dc1df5c5604368058a6aa92873e041b3ae3
IgorPis <pismestrovic.igor@gmail.com>
2025-02-20 17:02:46 +0100

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -123,6 +123,10 @@ async function controlAddRecipe(newRecipe) {
   }
 }
 
+function newFeature() {
+  console.log("Welcome to the application!");
+}
+
 function init() {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -131,6 +135,6 @@ function init() {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log("Welcome");
+  newFeature();
 }
 init();
